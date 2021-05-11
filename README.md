8e24271a061c1da4b984e1006c1aa3b7ff4e3036
陆云峰 <v-luyunfeng@xiaomi.com>
2021-05-11 15:19:18 +0800

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+English | [中文](./README-CN.md)
+
+<div align="center">
+
+# LARAVEL ELOQUENT TRAIT
+
+<p>
+    <a href="https://github.com/biiiiiigmonster/cleanable/blob/master/LICENSE"><img src="https://img.shields.io/badge/license-MIT-7389D8.svg?style=flat" ></a>
+    <a href="https://github.com/biiiiiigmonster/cleanable/releases" ><img src="https://img.shields.io/github/release/biiiiiigmonster/cleanable.svg?color=4099DE" /></a> 
+    <a href="https://packagist.org/packages/biiiiiigmonster/cleanable"><img src="https://img.shields.io/packagist/dt/biiiiiigmonster/cleanable.svg?color=" /></a> 
+    <a><img src="https://img.shields.io/badge/php-8.0+-59a9f8.svg?style=flat" /></a> 
+</p>
+
+</div>
+
+
+
+# Environment
+
+- PHP >= 8
+- laravel >= 8
+
+
+# Installation
+
+```bash
+composer require biiiiiigmonster/cleanable
+```
+
+# License
+[MIT](./LICENSE)
---
+++ src/Cleanable.php
@@ -0,0 +1,120 @@
+<?php
+
+
+namespace App\Concerns;
+
+
+use Closure;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasOneThrough;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Database\Eloquent\Relations\Relation;
+
+/**
+ * 关联数据可清除
+ * @package App\Concerns
+ */
+trait Cleanable
+{
+    /**
+     * 自动注册
+     */
+    protected static function bootCleanable(): void
+    {
+        /** 针对10种不同的关联关系，严格意义上分为删除前处理和删除后处理，因此分别监听事件deleting,deleted */
+        static::deleting(function ($model) {
+            $cleanable = $model->getCleanable();
+            foreach ($cleanable as $relationName) {
+                $relation = $model->$relationName();
+                if (!$relation instanceof Relation) continue;
+
+                if ($relation instanceof BelongsTo) {//BelongsTo or MorphTo
+                    $model->getRelationValue($relationName)?->delete();
+                }
+            }
+        });
+        static::deleted(function ($model) {
+            $cleanable = $model->getCleanable();
+            foreach ($cleanable as $relationName) {
+                $relation = $model->$relationName();
+                if (!$relation instanceof Relation) continue;
+
+                if (
+                    $relation instanceof HasOne
+                    || $relation instanceof HasOneThrough
+                    || $relation instanceof MorphOne
+                ) {
+                    $model->getRelationValue($relationName)?->delete();
+                }
+                if (
+                    $relation instanceof HasMany
+                    || $relation instanceof HasManyThrough
+                    || $relation instanceof MorphMany
+                ) {
+                    $model->getRelationValue($relationName)->map(function ($item) {
+                        $item->delete();
+                    });
+                }
+                if ($relation instanceof BelongsToMany) {//BelongsToMany or MorphToMany
+                    $relation->detach();
+                }
+            }
+        });
+    }
+
+    /**
+     * Get cleanable
+     * @return array
+     */
+    public function getCleanable(): array
+    {
+        return $this->cleanable;
+    }
+
+    /**
+     * Set the cleanable attributes for the model.
+     *
+     * @param array $cleanable
+     * @return $this
+     */
+    public function setCleanable(array $cleanable)
+    {
+        $this->cleanable = $cleanable;
+
+        return $this;
+    }
+
+    /**
+     * Make the given, typically visible, attributes cleanable.
+     *
+     * @param array|string|null $cleanables
+     * @return $this
+     */
+    public function makeCleanable($cleanables)
+    {
+        $this->cleanable = array_merge(
+            $this->cleanable, is_array($cleanables) ? $cleanables : func_get_args()
+        );
+
+        return $this;
+    }
+
+    /**
+     * Make the given, typically visible, attributes cleanable if the given truth test passes.
+     *
+     * @param bool|Closure $condition
+     * @param array|string|null $cleanables
+     * @return $this
+     */
+    public function makeCleanableIf($condition, $cleanables)
+    {
+        $condition = $condition instanceof Closure ? $condition($this) : $condition;
+
+        return value($condition) ? $this->makeCleanable($cleanables) : $this;
+    }
+}
